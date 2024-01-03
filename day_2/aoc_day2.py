5f278c4dea8a1ffdf83d20417e0e8bf61a5a2195
Will Hutzel <whutzel89@gmail.com>
2024-01-02 23:30:57 -0800

    updated day2 aoc to parse list of color and number

--- day_2/aoc_day2.py
+++ day_2/aoc_day2.py
@@ -4,5 +4,12 @@ def test(file_in):
     sum = 0
 
     for line in Lines:
+        game_dict = {'red':[],'blue':[],'green':[]}
         game = line.split(':')[0].split(' ')[1]
-        color = line.replace(';',',').split(': ')[1].split(', ')
+        colors = line.replace(';',',').split(': ')[1].split(', ')
+        (game_dict[color.split(' ')[1].replace('\n','')].append(color.split(' ')[0]) for color in colors)
+        print(game_dict)
+        #print(int(color.split(' ')[0]))
+
+if __name__ == "__main__":
+    test('/Users/whutzel/local_code/development/python/aoc_2023/day_2/example1.txt')
