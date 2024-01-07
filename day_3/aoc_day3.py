ef6506033bb9e0292f85c0a1169bb0a8c9e373af
Will Hutzel <whutzel89@gmail.com>
2024-01-06 22:55:43 -0800

    attempting day 3 aoc code

--- day_3/aoc_day3.py
+++ day_3/aoc_day3.py
@@ -1,2 +1,18 @@
+def test(in_file):
+    file1 = open(in_file, 'r')
+    Lines = file1.readlines()
+    
+    for line in Lines:
+        #print(line)
+        #s = [int(s) for s in str.split(line,'.') if s.isdigit()]
+        #print(s)
+        s = []
+        for t in line.split('.'):
+            try:
+                s.append(float(t))
+            except ValueError:
+                pass
+        print(s)
+        
 if __name__ == "__main__":
-    print('hello')
+    test('/Users/whutzel/local_code/development/python/aoc_2023/day_3/example1.txt')
