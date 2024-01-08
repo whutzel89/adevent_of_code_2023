f5550fdba2e6f5f43472aa2fc8acfdfaf69414b7
Will Hutzel <whutzel89@gmail.com>
2024-01-07 21:02:21 -0800

    added some notes to day3 code, came u with plan

--- day_3/aoc_day3.py
+++ day_3/aoc_day3.py
@@ -4,15 +4,18 @@ def test(in_file):
     
     for line in Lines:
         #print(line)
-        #s = [int(s) for s in str.split(line,'.') if s.isdigit()]
-        #print(s)
-        s = []
-        for t in line.split('.'):
-            try:
-                s.append(float(t))
-            except ValueError:
-                pass
+        s = [i for i,s in enumerate(line) if s.isdigit()] # this finds index of numbers
+        # I think the next step will be to get consecutive integres from list, one method would be to iterate through list and check if next
+        # value is +1 to current and append to list this should return a list of lists, [[0,1,2], [5,6,7],[12,13]]. Take these indexes then
+        # check left and right of max min and below/above range
         print(s)
+        #s = []
+        #for t in line.split('.'):
+        #    try:
+        #        s.append(float(t))
+        #    except ValueError:
+        #        pass
+        #print(s)
         
 if __name__ == "__main__":
     test('/Users/whutzel/local_code/development/python/aoc_2023/day_3/example1.txt')
